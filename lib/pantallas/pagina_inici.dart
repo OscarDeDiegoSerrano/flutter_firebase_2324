a0a6a34418da8fe62ab9f37a9e25e2d53c4f2adb
Oscar <oscarde_diego@ceroca.cat>
2024-03-01 20:26:39 +0100

    PortalAuth fet i serveiAuth iniciat

---
+++ lib/pantallas/pagina_inici.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PaginaInici extends StatelessWidget {
+  const PaginaInici({super.key});
+
+  void LogOut(){
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pagina Inicial"),
+        actions: [
+
+        ],
+      )
+    );
+  }
+}
